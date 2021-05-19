0c722fbbf2fdeeb017deabeb6d10b970b5b2b8ed
Amanda Davitt <acdavitt@gmail.com>
2021-05-18 22:50:05 -0500

    Created SearchResults and UserList

--- client/components/Lists/SearchResults.jsx
+++ client/components/Lists/SearchResults.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import AddLocationIcon from '@material-ui/icons/AddLocation';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 100
+  }
+}))
 
 const SearchResults = () => {
-  return <p> this will be SearchResults</p>
+  const classes = useStyles();
+  return (
+    <List className={classes.root}> Search Results
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="POI name" src="thumbnail" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="POI name"
+          secondary={
+            <>
+              {"snippet"}
+              {"link"}
+            </>
+          }
+        />
+        <ListItemSecondaryAction>
+          <IconButton aria-label="Add Location">
+            <AddLocationIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+
+    </List>
+  )
 }
 
 export default SearchResults;
--- client/components/Lists/UserList.jsx
+++ client/components/Lists/UserList.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 100
+  }
+}))
+
 
 const UserList = () => {
-  return <p>this will be the UserList</p>
+
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>Saved Places
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="POI name" src="thumbnail" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="POI name"
+          secondary={
+            <>
+              {"snippet"}
+              {"link"}
+            </>
+          }
+        />
+      </ListItem>
+
+    </List>
+  )
 }
 
 export default UserList;
